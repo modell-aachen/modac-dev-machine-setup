2bb6e97fb5622db919d57e68a2c2d7d47d4a2cf5
Marco Moscher <marco.moscher@rwth-aachen.de>
2019-10-12 18:22:36 +0200

    rework; fixed autocompletion; added vault

--- setup.sh
+++ setup.sh
@@ -6,7 +6,11 @@ sudo apt update
 sudo apt install --yes software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install --yes ansible
+echo ">> Done.\n"
 
-echo ">> Done."
 echo ">> Install Ansible Roles using ansible-galaxy"
 ansible-galaxy install -p ./roles -r requirements.yml
+echo ">> Done.\n"
+
+echo ">> Start provisioning"
+ansible-playbook -K playbook.yml
