b8fdce776f86d1d7bb09078b1b7d76cf02ca7e9d
Johannes Roos <quitejonny@users.noreply.github.com>
2025-02-24 10:50:27 +0100

    Installs poetry via bash (#242)

---
+++ devbox/provision-scripts/poetry.bash
@@ -0,0 +1,5 @@
+if [ -f "$HOME/.local/bin/poetry" ] && [[ "$($HOME/.local/bin/poetry --version)" == *"$POETRY_VERSION"* ]]; then
+    echo "poetry in version $POETRY_VERSION is already installed"
+else
+    curl -sSL https://install.python-poetry.org | python3 - --version $POETRY_VERSION
+fi
