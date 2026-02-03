39269f5d923f2a2464d14b89656de9d4c277bc96
André Tebart <andre.tebart@modell-aachen.de>
2026-02-03 10:19:07 +0100

    Remove devspace auto-completions

--- nixpkgs/modac-dev-provisioner/share/modac-dev-provisioner/provision-scripts/completions.bash
+++ nixpkgs/modac-dev-provisioner/share/modac-dev-provisioner/provision-scripts/completions.bash
@@ -23,7 +23,6 @@ for shell in bash zsh; do
     install_completion "$shell" flux 2_5_1
     install_completion "$shell" op 2_30_3
     install_completion "$shell" helm 3_17_3
-    install_completion "$shell" devspace 6_3_15
     install_completion "$shell" kubectl 1_32_3
 done
 
